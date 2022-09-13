a5e5957493b51a7feac8c3e00081e054947f9231
Long Phu <phu.huynh@monimedia.com>
2022-09-13 10:39:58 +0000

    Init Devcontainer

---
+++ check.sh
@@ -0,0 +1,29 @@
+#! /bash/bin
+echo "==================================="
+echo "Current folder [pwd]:"
+pwd
+
+echo "==================================="
+echo "Who am I? [whoami]"
+whoami
+
+echo "==================================="
+echo "Go Version [go version]"
+go version
+
+echo "==================================="
+echo "Python Version [python --version]"
+python --version
+
+echo "==================================="
+echo "Rust Version [rustup --version] [rustc --version] [cargo --version]"
+rustup --version
+rustc --version
+cargo --version
+
+echo "==================================="
+echo "Docker Version [docker --version]"
+docker --version
+
+echo "==================================="
+
